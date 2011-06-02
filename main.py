034443f1b79a48f89a0e0d0f330c7644ad7772c5
Shinya Okano <tokibito@gmail.com>
2011-06-02 18:57:30 +0900

    追加した機能の設定をmain.pyテンプレートに追加

--- main.py
+++ main.py
@@ -4,13 +4,31 @@ from hgwebcommit import app
 # config
 SECRET_KEY = '(secret key)'
 HGWEBCOMMIT_REPOSITORY = '/path/to/repository'
+#HGWEBCOMMIT_REPOSITORY_BACKEND = 'hgwebcommit.repository.dummy.DummyRepository'
 HGWEBCOMMIT_ENCODING = 'utf-8'
 HGWEBCOMMIT_ALLOW_COMMIT = True
 
 # actions
 HGWEBCOMMIT_ACTIONS = (
-    # 'hgwebcommit.actions.hello',
+    #'hgwebcommit.actions.hello',
 )
+
+# command actions
+#from hgwebcommit.actions import ExecuteCommandAction, manager
+#act = ExecuteCommandAction('echo_command', 'echo command', ['echo', 'hello'])
+#manager.add(act)
+
+# logging
+#import logging
+#from hgwebcommit.logger import MonthlyRotatingFileHandler
+#logging_handler = MonthlyRotatingFileHandler('/var/log/webcommit.log')
+#logging_handler.setLevel(logging.INFO)
+#logging_handler.suffix = '%Y%m'
+#logging_handler.setFormatter(logging.Formatter(
+#    '%(asctime)s %(message)s',
+#    '%Y-%m-%d %H:%M:%S'
+#))
+#app.logger.addHandler(logging_handler)
 
 if __name__ == '__main__':
     app.debug = True
